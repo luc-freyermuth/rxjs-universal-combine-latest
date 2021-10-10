9e5ed2b157254b47b37adafcdeabfb9def032657
Luc Freyermuth <lucfreyermuth@gmail.com>
2021-10-11 00:26:13 +0200

    tuples types up to 8 long

--- src/universal-combine-latest.types.ts
+++ src/universal-combine-latest.types.ts
@@ -2,7 +2,56 @@ import { Observable } from 'rxjs';
 
 type ExtractObservableType<T> = T extends Observable<infer U> ? U : T;
 
-type ExtractObservableArrayTypes<T extends any[]> = T extends [infer O1, infer O2, infer O3]
+type ExtractObservableArrayTypes<T extends any[]> = T extends [
+  infer O1,
+  infer O2,
+  infer O3,
+  infer O4,
+  infer O5,
+  infer O6,
+  infer O7,
+  infer O8
+]
+  ? [
+      ExtractObservables<O1>,
+      ExtractObservables<O2>,
+      ExtractObservables<O3>,
+      ExtractObservables<O4>,
+      ExtractObservables<O5>,
+      ExtractObservables<O6>,
+      ExtractObservables<O7>,
+      ExtractObservables<O8>
+    ]
+  : T extends [infer O1, infer O2, infer O3, infer O4, infer O5, infer O6, infer O7]
+  ? [
+      ExtractObservables<O1>,
+      ExtractObservables<O2>,
+      ExtractObservables<O3>,
+      ExtractObservables<O4>,
+      ExtractObservables<O5>,
+      ExtractObservables<O6>,
+      ExtractObservables<O7>
+    ]
+  : T extends [infer O1, infer O2, infer O3, infer O4, infer O5, infer O6]
+  ? [
+      ExtractObservables<O1>,
+      ExtractObservables<O2>,
+      ExtractObservables<O3>,
+      ExtractObservables<O4>,
+      ExtractObservables<O5>,
+      ExtractObservables<O6>
+    ]
+  : T extends [infer O1, infer O2, infer O3, infer O4, infer O5]
+  ? [
+      ExtractObservables<O1>,
+      ExtractObservables<O2>,
+      ExtractObservables<O3>,
+      ExtractObservables<O4>,
+      ExtractObservables<O5>
+    ]
+  : T extends [infer O1, infer O2, infer O3, infer O4]
+  ? [ExtractObservables<O1>, ExtractObservables<O2>, ExtractObservables<O3>, ExtractObservables<O4>]
+  : T extends [infer O1, infer O2, infer O3]
   ? [ExtractObservables<O1>, ExtractObservables<O2>, ExtractObservables<O3>]
   : T extends [infer O1, infer O2]
   ? [ExtractObservables<O1>, ExtractObservables<O2>]
